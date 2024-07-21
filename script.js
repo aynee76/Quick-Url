eda9e12fe589b43279c05c6d25bd19b2ecc17957
aynee76 <abdulmuminzainab2@gmail.com>
2024-07-21 01:49:54 +0100

    update

---
+++ script.js
@@ -0,0 +1,112 @@
+const bookmarkForm = document.getElementById("bookmarkForm");
+const siteNameInput = document.getElementById("siteName");
+const siteUrlInput = document.getElementById("siteUrl");
+const bookmarkList = document.getElementById("bookmarkList");
+
+bookmarkForm.addEventListener("submit", addBookmark);
+
+document.addEventListener("DOMContentLoaded", displayBookmarks);
+
+function addBookmark(event) {
+    event.preventDefault();
+
+    const siteName = siteNameInput.value;
+    const siteUrl = siteUrlInput.value;
+
+    if(!validateForm(siteName, siteUrl)) {
+        return;
+    }
+
+    if(isDuplicateBookmark(siteName, siteUrl)) {
+        alert("This bookmark already exist");
+        return;
+    }
+
+    const bookmark = {
+        name:siteName,
+        url: siteUrl,
+    };
+
+    saveBookmark(bookmark);
+
+    displayBookmarks(bookmark);
+
+    siteNameInput.value = ""
+    siteUrlInput.value = ""
+}
+
+function validateForm(siteName, siteUrl) {
+    if(!siteName || !siteUrl) {
+        alert("Please fill in both fields")
+        return;
+    }
+
+    try {
+        new URL(siteUrl)
+        return true;
+    }
+
+    catch(error) {
+        alert("Please enter a valid URL")
+        return false;
+    }
+}
+
+function displayBookmark(bookmark) {
+    const bookmarkItem = document.createElement("li");
+    const link = document.createElement("a");
+    link.href = bookmark.url;
+    link.textContent = bookmark.name;
+    link.target = "_blank";
+
+
+    // removal of items
+
+    const removeButton = document.createElement("button");
+    removeButton.innerHTML = '<i class="fas fa-trash-alt"></i>'
+
+    removeButton.addEventListener("click", () => removeBookmark(bookmark));
+
+
+    bookmarkItem.appendChild(link);
+    bookmarkItem.appendChild(removeButton);
+    bookmarkList.appendChild(bookmarkItem);
+}
+
+function saveBookmark(bookmark) {
+    let bookmarks = getBookmarks();
+    bookmarks.push(bookmark);
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks))
+}
+
+function getBookmarks() {
+    return JSON.parse(localStorage.getItem("bookmarks")) || [];
+}
+
+function displayBookmarks(){
+    bookmarkList.innerHTML = "";
+    let bookmarks = getBookmarks();
+    bookmarks.forEach((bookmark) => {
+        displayBookmark(bookmark);
+    })
+}
+
+function isDuplicateBookmark(siteName, siteUrl) {
+    let bookmarks = getBookmarks();
+
+    return bookmarks.some(
+        (bookmark) => bookmark.name.toLowerCase() === siteName.toLowerCase() && bookmark.url.toLowerCase() === siteUrl.toLowerCase()
+    );
+}
+
+function removeBookmark(bookmark) {
+    let bookmarks = getBookmarks();
+    bookmarks = bookmarks.filter(
+        (b) => 
+        b.name.toLowerCase() !== bookmark.name.toLowerCase() || 
+        b.url.toLowerCase() !== bookmark.url.toLowerCase());
+
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+    bookmarkList.innerHTML = ""; 
+    displayBookmarks();
+}
